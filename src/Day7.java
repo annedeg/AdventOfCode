c52c183b6441960490b0c1da0dbae7c8a58b73a4
Anne de Graaff <anne.de.graaff@sogeti.com>
2023-12-07 17:51:18 +0100

    day 7

--- src/Day7.java
+++ src/Day7.java
@@ -2,126 +2,125 @@ import helpers.Directory;
 import helpers.File;
 import helpers.Helper;
 
-import java.util.LinkedHashSet;
-import java.util.LinkedList;
+import java.lang.reflect.Array;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Day7 extends CodeDay {
     @Override
     public void puzzleOne() {
         var input = Helper.readToStringArrayList("src/input/dayseven.txt");
+        ArrayList<Hand> collect = input.stream()
+            .map(Hand::new)
+            .sorted()
+            .collect(Collectors.toCollection(ArrayList::new));
 
-        Directory main = new Directory("/");
-        Directory currentDic = main;
-        for (var inputRow : input) {
-            System.out.println(inputRow);
-            if (inputRow.contains("$ ls")) {
-                continue;
-            }
-            if (inputRow.contains("dir")) {
-                var newDic = new Directory(inputRow.split("dir ")[1]);
-                newDic.setParent(currentDic);
-                currentDic.addFile(newDic);
-                continue;
-            }
+        Collections.reverse(collect);
 
-            if (inputRow.contains("$ cd")) {
-                String toWhere = inputRow.split("cd ")[1];
-                if (toWhere.equals("..")) {
-                    System.out.println("Current: " + currentDic.name);
-                    currentDic = currentDic.parent;
-                    System.out.println("Current: " + currentDic.name);
-                } else if(toWhere.equals("/")) {
-                    currentDic = main;
-                } else {
-                    currentDic = currentDic.openDirectory(toWhere);
-                }
-                continue;
-            }
+        long total = 0;
 
-            if (Integer.parseInt(inputRow.split(" ")[0]) > 0) {
-                currentDic.addFile(new File(inputRow.split(" ")[1], Integer.parseInt(inputRow.split(" ")[0])));
-            }
+        for (int rank = 0; rank < collect.size(); rank++) {
+            total += ((long) (rank + 1) * collect.get(rank).getWorth());
         }
 
-        int total = getSize(main);
         System.out.println(total);
-
     }
 
-    public int getSize(Directory directory) {
-        int total = 0;
-        for (var file : directory.files) {
-            if (file instanceof Directory dir) {
-                if (dir.getSize() <= 100000) {
-                    total += dir.getSize();
-                    for (var dict : dir.getAllDict()) {
-                        total += dict.getSize();
-                    }
+    class Hand implements Comparable {
+        ArrayList<Character> characters = Stream.of('2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A').collect(Collectors.toCollection(ArrayList::new));
+        int worth;
+        int type;
+        String cards;
+
+        Hand(String line) {
+            String[] s = line.split(" ");
+            worth = Integer.parseInt(s[1]);
+            cards = s[0];
+
+            HashMap<Character, Integer> map = new HashMap<>();
+            for (char c : s[0].toCharArray()) {
+                if (map.containsKey(c)) {
+                   map.put(c, map.get(c)+1);
+                   continue;
+                }
+
+                map.put(c, 1);
+            }
+
+            int size= map.keySet().size();
+            ArrayList<Integer> values = new ArrayList<>(map.values());
+
+            if (size == 1) {
+                type = 7;
+            }
+
+            if (size == 2) {
+                if (values.get(0) == 4 || values.get(1) == 4) {
+                    type = 6;
                 } else {
-                    total += getSize(dir);
+                    type = 5;
                 }
             }
+
+            if (size == 3) {
+                if (values.get(0) == 3 || values.get(1) == 3) {
+                    type = 4;
+                } else {
+                    type = 3;
+                }
+            }
+
+            if (size == 4) {
+                type = 2;
+            }
+
+            if (size == 5) {
+                type = 1;
+            }
         }
-        return total;
+
+        public int getType() {
+            return type;
+        }
+
+        private String getCards() {
+            return cards;
+        }
+
+        @Override
+        public int compareTo(Object o) {
+            if (!(o instanceof Hand h)) {
+                return 0;
+            }
+
+            if (h.getType() != this.getType()) {
+                return h.getType() - this.getType();
+            }
+
+            return better(h);
+        }
+
+        private int better(Hand h) {
+            char[] hChars = h.getCards().toCharArray();
+            char[] tChars = this.getCards().toCharArray();
+            for (int i = 0; i < hChars.length; i++) {
+                if (hChars[i] != tChars[i]) {
+                    return characters.indexOf(Character.valueOf(hChars[i])) - characters.indexOf(Character.valueOf(tChars[i]));
+                }
+            }
+
+            return 0;
+        }
+
+        private int getWorth() {
+            return worth;
+        }
     }
+
 
     @Override
     public void puzzleTwo() {
         var input = Helper.readToStringArrayList("src/input/dayseven.txt");
-
-        Directory main = new Directory("/");
-        Directory currentDic = main;
-        for (var inputRow : input) {
-//            System.out.println(inputRow);
-            if (inputRow.contains("$ ls")) {
-                continue;
-            }
-            if (inputRow.contains("dir")) {
-                var newDic = new Directory(inputRow.split("dir ")[1]);
-                newDic.setParent(currentDic);
-                currentDic.addFile(newDic);
-                continue;
-            }
-
-            if (inputRow.contains("$ cd")) {
-                String toWhere = inputRow.split("cd ")[1];
-                if (toWhere.equals("..")) {
-//                    System.out.println("Current: " + currentDic.name);
-                    currentDic = currentDic.parent;
-//                    System.out.println("Current: " + currentDic.name);
-                } else if(toWhere.equals("/")) {
-                    currentDic = main;
-                } else {
-                    currentDic = currentDic.openDirectory(toWhere);
-                }
-                continue;
-            }
-
-            if (Integer.parseInt(inputRow.split(" ")[0]) > 0) {
-                currentDic.addFile(new File(inputRow.split(" ")[1], Integer.parseInt(inputRow.split(" ")[0])));
-            }
-        }
-
-        int target = -(70000000 - 30000000 - main.getSize());
-        System.out.println("target" + target);
-        System.out.println(main.toString());
-    }
-
-    public int findLowest(Directory main, int target, int currentLowest) {
-        for (var file : main.files) {
-            if (file instanceof Directory directory) {
-                if (directory.getSize() > target && directory.getSize() < currentLowest) {
-                    currentLowest = directory.getSize();
-                } else {
-                    for (var newD : directory.getAllDict()) {
-                        int s = findLowest(newD, target, currentLowest);
-                        if (s < currentLowest) {
-                            currentLowest = s;
-                        }
-                    }
-                }
-            }
-        }
-        return currentLowest;
     }
 }
