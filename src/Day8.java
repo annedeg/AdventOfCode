5c0b361e1db9394a023aebb33373f48c787f7a92
Anne de Graaff <a.d.graaff@hotmail.com>
2023-12-08 14:26:40 +0100

    Finished day 8

--- src/Day8.java
+++ src/Day8.java
@@ -1,98 +1,50 @@
-import helpers.Directory;
-import helpers.File;
 import helpers.Helper;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Day8 extends CodeDay {
     @Override
     public void puzzleOne() {
         var input = Helper.readToStringArrayList("src/input/dayeight.txt");
-        int[][] map = toMap(input);
-        int total = 0;
-        for (int x = 0; x < map.length; x++) {
-            for (int y = 0; y < map[x].length; y++) {
-                if (y == 0 || x == 0 || y == map.length - 1 || x == map[x].length - 1) {
-                    total += 1;
-                    continue;
-                }
-                System.out.println(x + " " + y + " value: " + map[x][y]);
-                if (checkRow(x, y, Direction.LEFT, map) || checkRow(x, y, Direction.RIGHT, map) ||
-                        checkRow(x, y, Direction.UP, map) || checkRow(x, y, Direction.DOWN, map)) {
-                    total += 1;
-                }
+        String moves = input.get(0);
+        List<String> sequence = input.subList(2, input.size());
+
+        HashMap<String, ArrayList<String>> mapFromSequence = createMapFromSequence(sequence);
+
+        int maxMovesLength = moves.length() - 1;
+        int pointer = 0;
+        int counter = 0;
+        String currentKey = "AAA";
+        while (true) {
+            char move = moves.charAt(pointer);
+            ArrayList<String> strings = mapFromSequence.get(currentKey);
+            currentKey = strings.get((move == 'L') ? 0 : 1);
+            counter++;
+            if (currentKey.equals("ZZZ")) {
+                break;
             }
+
+            if (pointer == maxMovesLength) {
+                pointer = 0;
+                continue;
+            }
+
+            pointer += 1;
         }
-        System.out.println(total);
+        System.out.println(counter);
     }
 
-    enum Direction {UP, DOWN, LEFT, RIGHT}
+    public HashMap<String, ArrayList<String>> createMapFromSequence(List<String> sequence) {
+        HashMap<String, ArrayList<String>> map = new HashMap<>();
+        for (String row : sequence) {
+            String[] split = row.split(" = ");
+            String key = split[0];
+            String rawValue = split[1];
+            rawValue = rawValue.substring(1, rawValue.length() - 1);
+            ArrayList<String> value = Arrays.stream(rawValue.split(", ")).collect(Collectors.toCollection(ArrayList::new));
 
-    public boolean checkRow(int x, int y, Direction direction, int[][] map) {
-        var val = map[x][y];
-        switch (direction) {
-            case UP -> {
-                for (int yy = y - 1; yy >= 0; yy--) {
-                    if (((map[x][yy]) >= val)) {
-                        return false;
-                    }
-                }
-                System.out.println("trueup");
-                System.out.println("value: " + map[x][y]);
-                System.out.println();
-                return true;
-            }
-            case DOWN -> {
-                for (int yy = y + 1; yy <= map.length - 1; yy++) {
-                    if (((map[x][yy]) >= val)) {
-                        return false;
-                    }
-                }
-                System.out.println("truedown");
-                System.out.println("value: " + map[x][y]);
-                System.out.println();
-                return true;
-            }
-            case LEFT -> {
-                for (int xx = x - 1; xx >= 0; xx--) {
-                    if (((map[xx][y]) >= val)) {
-                        return false;
-                    }
-                }
-                System.out.println("trueleft");
-                System.out.println("value: " + map[x][y]);
-                System.out.println();
-                return true;
-            }
-            case RIGHT -> {
-                for (int xx = x + 1; xx <= map.length - 1; xx++) {
-                    if (((map[xx][y]) >= val)) {
-                        return false;
-                    }
-                }
-                System.out.println("trueright");
-                System.out.println("value: " + map[x][y]);
-                System.out.println();
-                return true;
-            }
-        }
-        return false;
-    }
-
-
-    public int[][] toMap(ArrayList<String> input) {
-
-        int[][] map = new int[input.size()][input.size()];
-
-        var xc = 0;
-        for (var row : input) {
-            var yc = 0;
-            for (char cr : row.toCharArray()) {
-                map[yc][xc] = Integer.parseInt(String.valueOf(cr));
-                yc++;
-            }
-            xc++;
+            map.put(key, value);
         }
 
         return map;
@@ -101,69 +53,63 @@ public class Day8 extends CodeDay {
     @Override
     public void puzzleTwo() {
         var input = Helper.readToStringArrayList("src/input/dayeight.txt");
-        int[][] map = toMap(input);
-        int total = 0;
-        for (int x = 0; x < map.length; x++) {
-            for (int y = 0; y < map[x].length; y++) {
-                System.out.println(x + " " + y + " value: " + checkRowValue(x,y,map));
-                if (checkRowValue(x, y, map) > total) {
-                    total = checkRowValue(x, y, map);
+        String moves = input.get(0);
+        List<String> sequence = input.subList(2, input.size());
+
+        HashMap<String, ArrayList<String>> mapFromSequence = createMapFromSequence(sequence);
+
+        int pointer = 0;
+        String[] currentKeys = mapFromSequence.keySet().stream().filter(key -> key.endsWith("A")).toArray(String[]::new);
+
+        long[] val = new long[6];
+        int i = -1;
+        for (String key : currentKeys) {
+            int c = 0;
+            i++;
+            while (true) {
+                if (key.endsWith("Z")) {
+                    val[i] = c;
+                    break;
+                }
+
+                key = mapFromSequence.get(key).get((moves.charAt(pointer) == 'L') ? 0 : 1);
+                c += 1;
+                if (pointer + 1 == moves.length()) {
+                    pointer = 0;
+                } else {
+                    pointer += 1;
                 }
             }
+
         }
-        System.out.println(total);
+
+        System.out.println(lcm(val));
     }
 
-    public int checkRowValue(int x, int y, int[][] map) {
-        var dist = 0;
-        var tempdist = 0;
-        var values = new int[4];
-        var val = map[x][y];
-
-        for (int yy = y - 1; yy >= 0; yy--) {
-            if (((map[x][yy]) >= val)) {
-                tempdist+=1;
-                break;
-            }
-            tempdist += 1;
-        }
-        values[0] = tempdist;
-        tempdist = 0;
-
-        for (int yy = y + 1; yy <= map.length - 1; yy++) {
-            if (((map[x][yy]) >= val)) {
-                tempdist+=1;
-                break;
-            }
-            tempdist += 1;
+    private long lcm(long... values) {
+        if (values.length == 2) {
+            return lcm(values[0], values[1]);
         }
 
-        values[1] = tempdist;
-        tempdist = 0;
+        long[] newValues = Arrays.copyOfRange(values, 1, values.length);
 
-        for (int xx = x - 1; xx >= 0; xx--) {
-            if (((map[xx][y]) >= val)) {
-                tempdist+=1;
-                break;
-            }
-            tempdist += 1;
+        long lcm = lcm(newValues);
+        long first = values[0];
+        return lcm(first, lcm);
+    }
+
+    public long lcm(long number1, long number2) {
+        if (number1 == 0 || number2 == 0) {
+            return 0;
         }
-
-        values[2] = tempdist;
-        tempdist = 0;
-
-        for (int xx = x + 1; xx <= map.length - 1; xx++) {
-            if (((map[xx][y]) >= val)) {
-                tempdist+=1;
-                break;
-            }
-            tempdist += 1;
+        long absNumber1 = Math.abs(number1);
+        long absNumber2 = Math.abs(number2);
+        long absHigherNumber = Math.max(absNumber1, absNumber2);
+        long absLowerNumber = Math.min(absNumber1, absNumber2);
+        long lcm = absHigherNumber;
+        while (lcm % absLowerNumber != 0) {
+            lcm += absHigherNumber;
         }
-
-        values[3] = tempdist;
-
-
-//        System.out.println(Arrays.stream(values).reduce(1, (a,b) -> a*b));
-        return Arrays.stream(values).reduce(1, (a,b) -> a*b);
+        return lcm;
     }
 }
