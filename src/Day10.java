8607107550a353aabb9b9853cd374291b06a675b
Anne de Graaff <a.d.graaff@hotmail.com>
2023-12-10 17:55:53 +0100

    day 9 and 10

--- src/Day10.java
+++ src/Day10.java
@@ -1,73 +1,263 @@
 import helpers.Helper;
 
 import java.util.ArrayList;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class Day10 extends CodeDay {
+    char[][] matrix;
+    ArrayList<ArrayList<Location>> allPaths = new ArrayList<>();
+    Location currentLocation = null;
+
     @Override
     public void puzzleOne() {
         var input = Helper.readToStringArrayList("src/input/dayten");
+        matrix = createMatrix(input);
 
-        int x = 1;
-        int totalSignalStrength = 0;
-        int c = 0;
+        Location start = findStart();
+        ArrayList<Location> history = new ArrayList<>();
+        ArrayList<Location> paths = findPath(start, start, history);
+        System.out.println(paths.size() / 2);
+    }
 
-        for (var cycle : input) {
-            if (cycle.contains("addx")) {
-                for (int i = 0; i < 2; i++) {
-                    c++;
-                    totalSignalStrength = doSignalStrength(c, x, totalSignalStrength);
+    public ArrayList<Location> findPath(Location currentLocation, Location start, ArrayList<Location> history) {
+
+        if (history.contains(currentLocation) || currentLocation.getChar() == '.') {
+            return history;
+        }
+
+        history.add(currentLocation);
+
+        ArrayList<Location> nextLocations = findNextLocations(currentLocation);
+
+        for (Location location : nextLocations) {
+            findPath(location, start, history);
+        }
+
+        return history;
+    }
+
+    public ArrayList<Location> findNextLocations(Location currentLocation) {
+        int x = currentLocation.x;
+        int y = currentLocation.y;
+
+        ArrayList<Location> locations = new ArrayList<>();
+        switch (matrix[x][y]) {
+            case '|' -> {
+                locations.add(new Location(x + 1, y));
+                locations.add(new Location(x - 1, y));
+            }
+            case '-' -> {
+                locations.add(new Location(x, y - 1));
+                locations.add(new Location(x, y + 1));
+            }
+            case 'L' -> {
+                locations.add(new Location(x, y + 1));
+                locations.add(new Location(x - 1, y));
+            }
+            case 'J' -> {
+                locations.add(new Location(x, y - 1));
+                locations.add(new Location(x - 1, y));
+            }
+            case '7' -> {
+                locations.add(new Location(x, y - 1));
+                locations.add(new Location(x + 1, y));
+            }
+            case 'F' -> {
+                locations.add(new Location(x, y + 1));
+                locations.add(new Location(x + 1, y));
+            }
+            case 'S' -> {
+                if (x + 1 < matrix.length && (matrix[x + 1][y] != '|' || matrix[x + 1][y] != '7' || matrix[x + 1][y] != 'F')) {
+                    locations.add(new Location(x + 1, y));
                 }
-                x+=Integer.parseInt(cycle.split("addx ")[1]);
-            } else {
-                c++;
-                totalSignalStrength = doSignalStrength(c, x, totalSignalStrength);
+                if (x > 0 && (matrix[x - 1][y] != '|' || matrix[x - 1][y] != 'L' || matrix[x - 1][y] != 'J')) {
+                    locations.add(new Location(x - 1, y));
+                }
+                if (y + 1 < matrix.length && (matrix[x][y + 1] != '-' || matrix[x][y + 1] != '7' || matrix[x][y + 1] != 'J')) {
+                    locations.add(new Location(x, y + 1));
+                }
+                if (y > 0 && (matrix[x][y - 1] != '-' || matrix[x][y - 1] != 'F' || matrix[x][y - 1] != 'L')) {
+                    locations.add(new Location(x, y - 1));
+                }
             }
         }
 
-        System.out.println(totalSignalStrength);
-    }
-
-    public int doSignalStrength(int c, int x, int totalSignalStrength) {
-        if (c == 20 || (c - 20) % 40 == 0)
-            totalSignalStrength += c*(x);
-
-        return totalSignalStrength;
+        ArrayList<Location> newLocations = locations.stream().filter(loc -> loc.getChar() != '.').collect(Collectors.toCollection(ArrayList::new));
+        return newLocations;
     }
 
     @Override
     public void puzzleTwo() {
         var input = Helper.readToStringArrayList("src/input/dayten");
-        int c = 0;
-        int x = 1;
-        ArrayList<ArrayList<String>> crtOutput = new ArrayList<>();
-        crtOutput.add(new ArrayList<>());
-        for (var cycle : input)
-            if (cycle.contains("addx")) {
-                for (int i = 0; i < 2; i++)
-                    c = doCycle(crtOutput, c, x);
-                x += Integer.parseInt(cycle.split("addx ")[1]);
-            } else {
-                c = doCycle(crtOutput, c, x);
+        matrix = createMatrix(input);
+
+        Location start = findStart();
+        ArrayList<Location> history = new ArrayList<>();
+        ArrayList<Location> path = findPath(start, start, history);
+
+        isInLoop(3, 14, history);
+        int amount = 0;
+        for (int x = 0; x < matrix.length; x++) {
+            for (int y = 0; y < matrix[0].length; y++) {
+                if (isInLoop(x, y, path)) {
+                    System.out.println(new Location(x, y));
+                    amount += 1;
+                }
+            }
+        }
+        System.out.println(amount);
+    }
+
+    public boolean isInLoop(int nx, int ny, ArrayList<Location> loop) {
+        boolean xCorrect = false;
+        boolean yCorrect = false;
+        int aantalX = 0;
+        int aantalY = 0;
+
+        boolean openx = false;
+        boolean openy = false;
+
+
+        Location toCheck = new Location(nx, ny);
+
+        if (loop.contains(toCheck) || nx == 0 || ny == 0 || nx == matrix.length-1 || ny == matrix[0].length-1) {
+            return false;
+        }
+
+        int direction = 0;
+        for (int x = 0; x < matrix.length; x++) {
+            Location location = new Location(x, ny);
+            Location next = new Location(x+1, ny);
+
+            if (openx && location.equals(toCheck)) {
+                xCorrect = true;
+                break;
+            } else if (location.equals(toCheck)) {
+                break;
             }
 
-        printCrt(crtOutput);
+            if (loop.contains(location) && (next.isPossible())) {
+                if (location.getChar() == 'F' || location.getChar() == 'J') {
+                    direction -= 1;
+                }
+
+                if (location.getChar() == '7' || location.getChar() == 'L') {
+                    direction += 1;
+                }
+
+                if (direction == 2 || direction == -2) {
+                    openx = !openx;
+                    direction = 0;
+                    continue;
+                }
+
+                if (location.getChar() == '-' && direction == 0) {
+                    openx = !openx;
+                }
+            }
+        }
+
+        direction = 0;
+
+        for (int y = 0; y < matrix[0].length; y++) {
+            Location location = new Location(nx, y);
+            Location next = new Location(nx, y + 1);
+
+            if (openy && location.equals(toCheck)) {
+                yCorrect = true;
+                break;
+            } else if (location.equals(toCheck)) {
+                break;
+            }
+
+            if (loop.contains(location) && (next.isPossible())) {
+                if (location.getChar() == 'F' || location.getChar() == 'J') {
+                    direction -= 1;
+                }
+
+                if (location.getChar() == '7' || location.getChar() == 'L') {
+                    direction += 1;
+                }
+
+                if (direction == 2 || direction == -2) {
+                    openy = !openy;
+                    direction = 0;
+                    continue;
+                }
+
+                if (location.getChar() == '|' && direction == 0) {
+                    openy = !openy;
+                }
+            }
+        }
+
+
+        return (xCorrect && yCorrect);
     }
 
-    public int doCycle(ArrayList<ArrayList<String>> crtOutput, int c, int x) {
-        var currentRow = crtOutput.get(crtOutput.size()-1);
-        writeToCurrent(currentRow, c, x);
-        if (++c % 40 == 0)
-            crtOutput.add(new ArrayList<>());
-        return c;
+    public char[][] createMatrix(ArrayList<String> input) {
+        char[][] matrix = new char[input.size()][input.get(0).length()];
+
+        int x = 0;
+        for (String inputLine : input) {
+            int y = 0;
+            for (char ch : inputLine.toCharArray()) {
+                matrix[x][y] = ch;
+                y++;
+            }
+            x++;
+        }
+
+        return matrix;
     }
 
-    public void printCrt(ArrayList<ArrayList<String>> crtOutput) {
-        for (var row : crtOutput) {
-            System.out.println(row.toString());
+    public Location findStart() {
+        for (int x = 0; x < matrix.length; x++) {
+            for (int y = 0; y < matrix[0].length; y++) {
+                if (matrix[x][y] == 'S') {
+                    return new Location(x, y);
+                }
+            }
+        }
+        return null;
+    }
+
+    public class Location {
+
+        private final int x;
+        private final int y;
+
+        Location(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public boolean isPossible() {
+            return this.x >= 0 && this.y >= 0 && this.x <= matrix.length && this.y <= matrix[0].length;
+        }
+
+        public char getChar() {
+            return matrix[x][y];
+        }
+
+        @Override
+        public String toString() {
+            return getChar() + " (" + x + "," + y + ")";
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Location location = (Location) o;
+            return x == location.x && y == location.y;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
         }
     }
-    private void writeToCurrent(ArrayList<String> currentRow, int c, int x) {
-        int cf = c % 40;
-        currentRow.add((cf == x || cf == x+1 || cf == x-1) ? "#" : ".");
-    }
 }
+
--- src/Day9.java
+++ src/Day9.java
@@ -1,233 +1,80 @@
 import helpers.Helper;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Objects;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Day9 extends CodeDay {
     @Override
     public void puzzleOne() {
         var input = Helper.readToStringArrayList("src/input/daynine.txt");
-
-        var h = new Position(0, 0);
-        var t = new Position(0, 0);
-        var allLocations = new ArrayList<Position>();
-        allLocations.add(t);
-        for (var c : input) {
-            var spl = c.split(" ");
-            var res = stepHead(spl[0], Integer.parseInt(spl[1]), h, t, allLocations);
-            h = (Position) res[0];
-            t = (Position) res[1];
-            allLocations = (ArrayList<Position>) res[2];
-            allLocations.add(t);
-        }
-
-//        allLocations.forEach(System.out::println);
-
-        var res = allLocations.stream()
-                .distinct().toList();
-//        System.out.println(res);
-        System.out.println(res.size());
+        long sum = input.stream()
+                .map(History::new)
+                .map(History::nextValue)
+                .mapToLong(a -> a)
+                .sum();
+        System.out.println(sum);
     }
 
-    public Object[] stepHead(String direction, int amount, Position h, Position t, ArrayList<Position> positions) {
-        for (int i = 0; i < amount; i++) {
-
-            var prevh = h;
-
-            h = switch (direction) {
-                case "R" -> new Position(h.x + 1, h.y);
-                case "L" -> new Position(h.x - 1, h.y);
-                case "U" -> new Position(h.x, h.y - 1);
-                case "D" -> new Position(h.x, h.y + 1);
-                default -> null;
-            };
-
-            t = stepTail(h, prevh, t);
-            System.out.println(h);
-            System.out.println(t + "\n");
-
-            positions.add(t);
-        }
-        return new Object[]{h, t, positions};
-    }
-
-    public Object[] stepHeadTwo(String direction, int amount, Position h, ArrayList<Position> t, ArrayList<Position> positions) {
-        for (int i = 0; i < amount; i++) {
-            var prevh = h;
-            h = switch (direction) {
-                case "R" -> new Position(h.x + 1, h.y);
-                case "L" -> new Position(h.x - 1, h.y);
-                case "U" -> new Position(h.x, h.y - 1);
-                case "D" -> new Position(h.x, h.y + 1);
-                default -> null;
-            };
-
-            t = setAllTails(h, prevh, t);
-
-//            printboard(t,h);
-            positions.add(t.get(8));
-        }
-
-        return new Object[]{h, t, positions};
-    }
-
-    private void printboard(ArrayList<Position> t, Position h) {
-        int map[][] = new int[10][10];
-
-        int c = 9;
-        for (var ti : t) {
-            map[ti.x+3][ti.y+5] = c;
-            c-=1;
-        }
-        map[h.x+3][h.y+5] = -1;
-
-        for (int x = 0; x < map.length; x++) {
-            var amounttoprint = 0;
-            var str = new StringBuilder();
-            for (int[] ints : map) {
-                if (ints[x] != 0) {
-                    amounttoprint += 1;
-                    if (ints[x] == -1) {
-                        str.append("H");
-                    } else {
-                        str.append(ints[x]);
-                    }
-                } else {
-                    str.append(".");
-                }
-            }
-//            if (amounttoprint > 0) {
-                System.out.println(str);
-//            }
-
-        }
-        System.out.println();
-        System.out.println();
-    }
-
-
-    public Position stepTail(Position h, Position prevh, Position t) {
-        int yc = (h.y - t.y);
-        int xc = (h.x - t.x);
-
-        if (Math.abs(yc) > 1 || Math.abs(xc) > 1) {
-            return prevh;
-        }
-
-        return t;
-    }
-
-    public ArrayList<Position> setAllTails(Position h, Position prevh, ArrayList<Position> t) {
-        for (int i = 0; i <= t.size()-1; i++) {
-            int yc = (h.y - t.get(i).y);
-            int xc = (h.x - t.get(i).x);
-            if (Math.abs(yc) > 1 || Math.abs(xc) > 1) {
-                if (xc == 0 && yc > 1) {
-                    t.set(i, new Position(t.get(i).x, t.get(i).y + 1));
-                }
-                if (xc == 0 && yc < -1) {
-                    t.set(i, new Position(t.get(i).x, t.get(i).y - 1));
-                }
-                if (yc == 0 && xc > 1) {
-                    t.set(i, new Position(t.get(i).x + 1, t.get(i).y));
-                }
-                if (yc == 0 && xc < -1) {
-                    t.set(i, new Position(t.get(i).x - 1, t.get(i).y));
-                }
-
-                if (Math.abs(xc) == 1 && yc > 1) {
-                    t.set(i, new Position(h.x, t.get(i).y + 1));
-                }
-                if (Math.abs(xc) == 1 && yc < -1) {
-                    t.set(i, new Position(h.x, t.get(i).y - 1));
-                }
-                if (Math.abs(yc) == 1 && xc > 1) {
-                    t.set(i, new Position(t.get(i).x + 1, h.y));
-                }
-                if (Math.abs(yc) == 1 && xc < -1) {
-                    t.set(i, new Position(t.get(i).x - 1, h.y));
-                }
-                if (yc == 2 && xc == 2) {
-                    t.set(i, new Position(t.get(i).x - 1, t.get(i).x - 1));
-                }
-
-                if (yc == -2 && xc == -2) {
-                    t.set(i, new Position(t.get(i).x + 1, t.get(i).y + 1));
-                }
-
-                if (yc == -2 && xc == 2) {
-                    t.set(i, new Position(t.get(i).x + 1, t.get(i).y - 1));
-                }
-
-                if (yc == 2 && xc == -2) {
-                    t.set(i, new Position(t.get(i).x - 1, t.get(i).y + 1));
-                }
-            }
-            h = t.get(i);
-        }
-
-        return t;
-    }
 
     @Override
     public void puzzleTwo() {
         var input = Helper.readToStringArrayList("src/input/daynine.txt");
-
-        var h = new Position(0, 0);
-        var t = new ArrayList<Position>();
-
-        for (int i = 0; i < 9; i++) {
-            t.add(new Position(0, 0));
+        ArrayList<String> reversed = new ArrayList<>();
+        for (String in : input) {
+            String[] split = in.split(" ");
+            for(int i = 0; i < split.length / 2; i++)
+            {
+                String temp = split[i];
+                split[i] = split[split.length - i - 1];
+                split[split.length - i - 1] = temp;
+            }
+            String join = String.join(" ", split);
+            reversed.add(join);
         }
 
-        var allLocations = new ArrayList<Position>();
-        allLocations.add(t.get(8));
-        for (var c : input) {
-            var spl = c.split(" ");
-            var res = stepHeadTwo(spl[0], Integer.parseInt(spl[1]), h, t, allLocations);
-            h = (Position) res[0];
-            t = (ArrayList<Position>) res[1];
-            allLocations = (ArrayList<Position>) res[2];
-            allLocations.add(t.get(8));
+
+        long sum = reversed.stream()
+                .map(History::new)
+                .map(History::nextValue)
+                .mapToLong(a -> a)
+                .sum();
+        System.out.println(sum);
+
+    }
+
+    class History {
+        ArrayList<ArrayList<Long>> history = new ArrayList<>();
+        String line;
+
+        public History(String line) {
+            this.line = line;
+            ArrayList<Long> firstLine = Arrays.stream(line.split(" ")).map(Long::parseLong).collect(Collectors.toCollection(ArrayList::new));
+            history.add(firstLine);
+
+            int p = 0;
+            while (!checkDone()) {
+                ArrayList<Long> nextLine = new ArrayList<>();
+                for (int i = 0; i < history.get(p).size()-1; i++) {
+                    long i1 = history.get(p).get(i + 1) - history.get(p).get(i);
+                    nextLine.add(i1);
+                }
+                p+=1;
+                history.add(nextLine);
+            }
         }
 
-//        allLocations.forEach(System.out::println);
+        private boolean checkDone() {
+            long sum = history.get(history.size() - 1).stream().filter(s -> s == 0).count();
+            return sum == history.get(history.size() - 1).size();
+        }
 
-        var res = allLocations.stream()
-                .distinct().toList();
-//        System.out.println(res);
-        System.out.println(res.size());
-    }
-}
+        public long nextValue() {
+            for (int p = history.size()-1; p >= 1; p--) {
+                long lastItem = history.get(p).get(history.get(p).size()-1);
+                history.get(p-1).add(lastItem + history.get(p-1).get(history.get(p-1).size()-1));
+            }
 
-class Position {
-    final int x;
-    final int y;
-
-    Position(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Position position)) return false;
-        return x == position.x && y == position.y;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(x, y);
-    }
-
-    @Override
-    public String toString() {
-        return "Position{" +
-                "x=" + x +
-                ", y=" + y +
-                '}';
+            return (history.get(0).get(history.get(0).size()-1));
+        }
     }
 }
