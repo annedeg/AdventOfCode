b009473d6f7f13a45ccd1e1690864857f4cff0b0
Anne de Graaff <a.d.graaff@hotmail.com>
2023-12-24 17:12:30 +0100

    Day 24 part one done

--- src/Day21.java
+++ src/Day21.java
@@ -3,6 +3,7 @@ import helpers.Helper;
 import javax.swing.text.Position;
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Day21 extends CodeDay {
     ArrayList<Position> positions = new ArrayList<Position>();
@@ -23,8 +24,13 @@ public class Day21 extends CodeDay {
             }
         }
 
-        for (int step = 0; step < 50; step++) {
-            positions = positions.stream().map(position -> position.getAllNextPositions(matrix)).flatMap(ArrayList::stream).filter(position -> position.valid(matrix)).distinct().collect(Collectors.toCollection(ArrayList::new));
+        for (int step = 0; step < 64; step++) {
+            positions = positions.stream()
+                    .map(position -> position.getAllNextPositions(matrix))
+                    .flatMap(ArrayList::stream)
+                    .filter(position -> position.valid(matrix))
+                    .distinct()
+                    .collect(Collectors.toCollection(ArrayList::new));
 
             for (int x = 0; x < matrix.length; x++) {
                 for (int y = 0; y < matrix[0].length; y++) {
@@ -98,6 +104,84 @@ public class Day21 extends CodeDay {
     }
     @Override
     public void puzzleTwo() {
+        var input = Helper.readToStringArrayList("src/input/day21");
+        char[][] matrix = input.stream()
+                .map(String::toCharArray)
+                .toArray(size -> new char[size][input.size()]);
 
+        int increase = 99;
+
+
+        for (int x = 0; x < matrix.length; x++) {
+            for (int y = 0; y < matrix[0].length; y++) {
+                if (matrix[x][y] == 'S') {
+                    matrix[x][y] = '.';
+                }
+            }
+        }
+
+        char[][] superMatrix = new char[matrix.length * increase][matrix[0].length*increase];
+
+        for (int rx = 0; rx < matrix.length*increase; rx++) {
+            for (int ry = 0; ry < matrix[0].length*increase;ry++) {
+                superMatrix[rx][ry] = matrix[rx % matrix.length][ry % matrix.length];
+            }
+        }
+
+        superMatrix[(superMatrix.length-1) / 2][(superMatrix[0].length-1) / 2] = 'S';
+
+        for (int x = 0; x < superMatrix.length; x++) {
+            for (int y = 0; y < superMatrix[0].length; y++) {
+                if (superMatrix[x][y] == 'S') {
+                    superMatrix[x][y] = '.';
+                    positions.add(new Position(x,y));
+                }
+            }
+        }
+
+//        for (char[] line : superMatrix) {
+//            for (char c : line) {
+//                System.out.print(c);
+//            }
+//            System.out.println();
+//        }
+
+        for (int step = 0; step < 100; step++) {
+            positions = positions.stream()
+                    .map(position -> position.getAllNextPositions(superMatrix))
+                    .flatMap(ArrayList::stream)
+                    .filter(position -> position.valid(superMatrix))
+                    .distinct()
+                    .collect(Collectors.toCollection(ArrayList::new));
+
+            for (int x = 0; x < superMatrix.length; x++) {
+                for (int y = 0; y < superMatrix[0].length; y++) {
+                    if (superMatrix[x][y] == 'O') {
+                        superMatrix[x][y] = '.';
+                    }
+
+                    if (positions.contains(new Position(x,y))) {
+                        superMatrix[x][y] = 'O';
+                    }
+                }
+            }
+
+            long count = 0;
+
+            for (int x = 0; x < superMatrix.length; x++) {
+                for (int y = 0; y < superMatrix[0].length; y++) {
+                    if (superMatrix[x][y] == 'O') {
+                        count++;
+                    }
+                }
+            }
+
+            System.out.println(count);
+        }
+
+
+
+
+//        System.out.println(count);
     }
 }
