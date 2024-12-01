5a85e57384813947b2a833fbe52d1eb21e82274c
Anne de Graaff <a.d.graaff@hotmail.com>
2024-12-01 12:44:25 +0100

    2024 day one

--- src/Day1.java
+++ src/Day1.java
@@ -1,78 +1,58 @@
+import helpers.Helper;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Day1 extends CodeDay {
-    @Override
+public class Day1 {
+    ArrayList<Integer> listOne;
+    ArrayList<Integer> listTwo;
+    public void handleInput() {
+        listOne = new ArrayList<>();
+        listTwo = new ArrayList<>();
+
+        ArrayList<String> rows = Helper.readToStringArrayList("C:\\Users\\adgra\\IdeaProjects\\AdventOfCode2022\\src\\input\\01");
+        rows.stream().map(row -> row.split(" {3}")).forEach(arr -> {
+            String s1 = arr[0];
+            String s2 = arr[1];
+
+            listOne.add(Integer.parseInt(s1));
+            listTwo.add(Integer.parseInt(s2));
+        });
+    }
     public void puzzleOne() {
-        ArrayList<ArrayList<Integer>> values;
-        try (var br = new BufferedReader(new FileReader("src/input/dayone.txt"))) {
-            values = new ArrayList<>();
-            values.add(new ArrayList<>());
-            int counter = 0;
-
-            int sum = 0;
-            String line;
-            while ((line = br.readLine()) != null) {
-                String nLine = line.replaceAll("[^\\d]", "");
-                String t = String.valueOf(nLine.substring(0, 1) + nLine.substring(nLine.length() - 1, nLine.length()));
-                sum += Integer.parseInt(t);
-            }
-
-            System.out.println(sum);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        handleInput();
+        List<Integer> sortedListOne = listOne.stream().sorted().toList();
+        List<Integer> sortedListTwo = listTwo.stream().sorted().toList();
+        int total = 0;
+        for (int i = 0; i < sortedListOne.size(); i++) {
+            total += Math.abs(sortedListOne.get(i) - sortedListTwo.get(i));
         }
+        System.out.println(total);
     }
 
-    @Override
     public void puzzleTwo() {
-        ArrayList<ArrayList<Integer>> values;
-        try (var br = new BufferedReader(new FileReader("src/input/dayone.txt"))) {
-            values = new ArrayList<>();
-            values.add(new ArrayList<>());
-            int counter = 0;
-
-            int sum = 0;
-            String line;
-            while ((line = br.readLine()) != null) {
-                Pattern pattern = Pattern.compile("[1-9]");
-
-                line = line.replaceAll("one", "on1e");
-                line = line.replaceAll("two", "tw2o");
-                line = line.replaceAll("three", "thr3e");
-                line = line.replaceAll("four", "fo4ur");
-                line = line.replaceAll("five", "fi5ve");
-                line = line.replaceAll("six", "si6x");
-                line = line.replaceAll("seven", "sev7en");
-                line = line.replaceAll("eight", "eigh8t");
-                line = line.replaceAll("nine", "nin9e");
-
-                Matcher matcher = pattern.matcher(line);
-
-                ArrayList<String> found = new ArrayList<>();
-                while (matcher.find()) {
-                    String group = matcher.group(0);
-                    if (group == null || group.length() == 0) {
-                        continue;
-                    }
-
-                    found.add(group);
-                }
-
-
-                String s = found.get(0) + found.get(found.size()-1);
-                sum += Integer.parseInt(s);
-            }
-
-            System.out.println(sum);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        handleInput();
+        int total = 0;
+        for (int i = 0; i < listOne.size(); i++) {
+            total += listOne.get(i) * sim(listTwo, listOne.get(i));
         }
+        System.out.println(total);
+    }
+
+    public int sim(List<Integer> list, int num) {
+        return (int) list.stream().filter(integer -> integer == num).count();
+    }
+
+    public static void main(String[] args) {
+        Day1 day1 = new Day1();
+        day1.puzzleOne();
+        day1.puzzleTwo();
     }
 }
